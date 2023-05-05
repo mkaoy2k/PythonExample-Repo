29dfeeb504bb3149d7b953f50073e044f7a186b7
Michael Kao <mkaoy2k@gmail.com>
2023-05-04 18:28:48 -0700

    Added functional test for Binary & Binomial heaps

--- heapBinary.py
+++ heapBinary.py
@@ -1,26 +1,58 @@
+"""
+Implementin a Binary Heap using Python list.
+"""
 class BinaryHeap:
     def __init__(self):
+        """
+        Initializes an empty heap list.
+        """
         self.heap = []
 
+    def __repr__(self):
+        """Dump Binary Heap"""
+        str_dump = f'Dump Binary Heap={self.heap}\n'
+
+        return str_dump
+    
     def parent(self, i):
+        """
+        Returns the index of the parent node of the node at index `i`.
+        """
         return (i - 1) // 2
 
     def leftChild(self, i):
+        """
+        Returns the index of the left child of the node at index `i`.
+	    """
         return 2*i + 1
 
     def rightChild(self, i):
+        """
+        Returns the index of the right child of the node at index `i`.
+        """
         return 2*i + 2
 
-    def getMin(self):
+    def find_min(self):
+        """
+        Returns the minimum value in the heap.
+        """
         if len(self.heap) == 0:
             return None
         return self.heap[0]
 
     def insert(self, value):
+        """
+        Inserts a new value into the heap and 
+        maintains the heap property by calling `heapifyUp`.
+        """
         self.heap.append(value)
         self.heapifyUp(len(self.heap) - 1)
 
-    def deleteMin(self):
+    def extract_min(self):
+        """
+        Removes and returns the minimum value in the heap and 
+        maintains the heap property by calling `heapifyDown`.
+        """
         if len(self.heap) == 0:
             return None
         if len(self.heap) == 1:
@@ -31,11 +63,24 @@ class BinaryHeap:
         return min_value
 
     def heapifyUp(self, i):
+        """
+        Maintains the heap property after adding a new value to the heap. 
+        If the parent node has a value that is greater than the current node, 
+        the current node is swapped with the parent node.
+        And the process is repeated until the parent node is smaller than 
+        the current node.
+        """
         while i > 0 and self.heap[i] < self.heap[self.parent(i)]:
             self.heap[i], self.heap[self.parent(i)] = self.heap[self.parent(i)], self.heap[i]
             i = self.parent(i)
 
     def heapifyDown(self, i):
+        """
+        Maintains the heap property after removing a value from the heap. 
+        The current node is swapped with its smallest child node 
+        until it is smaller than both of its children 
+        or it reaches a leaf node.
+        """
         while self.leftChild(i) < len(self.heap):
             min_child = self.leftChild(i)
             if self.rightChild(i) < len(self.heap) and self.heap[self.rightChild(i)] < self.heap[min_child]:
@@ -44,3 +89,29 @@ class BinaryHeap:
                 break
             self.heap[i], self.heap[min_child] = self.heap[min_child], self.heap[i]
             i = min_child
+# Functional Test
+if __name__ == '__main__':
+    H = BinaryHeap()
+
+    H.insert(10)
+    print('===> 10 inserted')
+    H.insert(2)
+    print('===> 2 inserted')
+    H.insert(15)
+    print('===> 15 inserted')
+    H.insert(6)
+    print('===> 6 inserted')
+
+    m = H.find_min()
+    print('Find min of 2 expected')
+    print(f'{m} from {H}')  # 2
+
+    q = H.extract_min()
+    print('Extract min of 2 expected')
+    print(f'{q} from {H}')   # 2
+
+    q = H.extract_min()
+    print('Extract min of 6 expected')
+    print(f'{q} from {H}')   # 6
+
+
--- heapBinomial.py
+++ heapBinomial.py
@@ -1,35 +1,83 @@
+"""
+This implementation consists of two classes, `Node` and `BinomialHeap`.
+Each `Node` contains a key value and pointers to its parent and children nodes. The `BinomialHeap` class is implemented with a list `trees` which contains the individual trees that make up the heap.
+
+This implementation can be improved for efficiency, but it should provide 
+a basic understanding of how a binomial heap works in Python.
+"""
 class Node:
     def __init__(self, key):
         self.key = key
         self.parent = None
         self.children = []
 
+    def __repr__(self):
+        """Dump Binomial Node"""
+        str_dump = f'Dump Node key={self.key}\n'
+        str_dump += f'===> parent={self.parent}\n'
+
+        return str_dump
 class BinomialHeap:
     def __init__(self):
         self.trees = []
     
+    def find_min(self):
+        """
+        The `find_min` method 
+        returns the minimum key node in the heap.
+        """
+        if not self.trees:
+            return None
+        min_node = min(self.trees, key=lambda x: x.key)
+        return min_node
+
     def insert(self, key):
+        """
+        The `insert` method 
+        creates a new node, 
+        adds it to a new heap, and 
+        merges the new heap with the existing heap.
+        """
         new_node = Node(key)
         new_heap = BinomialHeap()
         new_heap.trees.append(new_node)
         self.merge(new_heap)
     
     def extract_min(self):
+        """
+        The `extract_min` method 
+        removes the minimum key node from the heap, 
+        sets its children to a new heap, 
+        merges the new heap with the existing heap,
+        return the minimum key node.
+        """
         if not self.trees:
             return None
         min_node = min(self.trees, key=lambda x: x.key)
+        
         self.trees.remove(min_node)
         new_heap = BinomialHeap()
         new_heap.trees = min_node.children[:]
         self.merge(new_heap)
-        return min_node.key
+        return min_node
     
     def merge(self, other_heap):
+        """
+        The `merge` method 
+        merges two heaps by concatenating their tree lists and 
+        sorting the list by the number of children in each tree. 
+        This ensures that trees of the same order are combined correctly.
+        """
         self.trees += other_heap.trees
         self.trees.sort(key=lambda x: len(x.children))
         self.combine_trees()
     
     def combine_trees(self):
+        """
+        The `combine_trees` method 
+        iterates over the sorted list of trees and 
+        combines trees of equal order by making one tree the child of the other.
+        """
         if not self.trees:
             return
         combined = [self.trees.pop(0)]
@@ -40,3 +88,28 @@ class BinomialHeap:
                 combined[-1].children.append(tree)
                 tree.parent = combined[-1]
         self.trees = combined[:]
+# Functional Test
+if __name__ == '__main__':
+    H = BinomialHeap()
+
+    H.insert(10)
+    print('===> 10 inserted')
+    H.insert(2)
+    print('===> 2 inserted')
+    H.insert(15)
+    print('===> 15 inserted')
+    H.insert(6)
+    print('===> 6 inserted')
+
+    m = H.find_min()
+    print('Find min of 2 expected')
+    print(f'{m}')  # 2
+
+    q = H.extract_min()
+    print('Extract min of 2 expected')
+    print(f'{q}')   # 2
+
+    q = H.extract_min()
+    print('Extract min of 6 expected')
+    print(f'{q}')   # 6
+
